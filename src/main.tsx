87fc6544faac697024598521c13a95b75999740f
Fifinets <filipbosz007@gmail.com>
2025-08-04 19:18:35 +0200

    fix: add Clerk publishable key

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
 import './index.css'
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={clerkPubKey} afterSignOutUrl="/">
+createRoot(document.getElementById('root')!).render(
+  <ClerkProvider publishableKey="pk_test_Y3JlZGlibGUtY293LTI5LmNsZXJrLmFjY291bnRzLmRldiQ" afterSignOutUrl="/">
     <App />
   </ClerkProvider>
 )
